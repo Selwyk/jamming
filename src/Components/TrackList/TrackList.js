d1630affe6718bb26e4a693aae2568632ec8d1a5
Victor Suárez Gómez <victor.suarez.gomez@avanade.com>
2018-06-27 19:34:37 +0200

    First commit

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './TrackList.css';
+import Track from './Components/Track/Track';
+
+class TrackList extends Component{
+  render(){
+    return(
+      <div className="TrackList">
+          <!-- You will add a map method that renders a set of Track components  -->
+      </div>
+    )}
+  }
+
+export default TrackList;
